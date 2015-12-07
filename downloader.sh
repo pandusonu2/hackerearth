9aa8035bd32abe6e6fe88698158aec3c9915b5b4
pandusonu2 <pandu.sonu@yahoo.com>
2015-12-07 12:45:52 +0530

    A few changes

--- downloader.sh
+++ downloader.sh
@@ -1,13 +1,10 @@
-grep 'more<' index.html > jump.txt
-sort -u -o jump.txt jump.txt
-noLines=$(wc -l < jump.txt)
-for i in $(seq 1 $((noLines)));
+grep 'more<' index.html | uniq | 
+while read -r link
 do
-  link="$(sed -n "${i}p" jump.txt)"
   link=${link// }
   link=${link%% }
   starti="$(echo $link | grep -aob / | grep -oE '[0-9]+' | sed "2q;d")"
-	endi="$(echo $link | grep -aob '/' | grep -oE '[0-9]+' | sed "3q;d")"
+    endi="$(echo $link | grep -aob '/' | grep -oE '[0-9]+' | sed "3q;d")"
   length=$((endi-starti))
 	output=${link:$((starti+1)):$((length-1))}
   starti="$(echo $link | grep -aob '"' | grep -oE '[0-9]+' | sed "1q;d")"
@@ -15,8 +12,9 @@ do
   length=$((endi-starti))
 	link2=${link:$((starti+1)):$((length-1))}
   page="www.hackerearth.com$link2"
-  wget -O $output.html $page
+  echo $output
+  wget -o log.txt -O $output.html $page
   html2pdf $output.html $output.pdf
-#  gnome-open $output.pdf
   rm $output.html
 done
+rm log.txt
